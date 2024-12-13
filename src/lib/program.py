48ce14955823aca0852c0ae06a04613e573ac5fc
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-12-13 15:33:16 +0100

    Updated main

--- src/lib/program.py
+++ src/lib/program.py
@@ -8,6 +8,6 @@ from lib.mylogger import MyLogger
 class Program:
     args: dict = field(default_factory=dict)
     logger: MyLogger | None = field(default_factory=None)
-    ROOT_DIR = Path(__file__).parent.parent
+    ROOT_DIR = Path(__file__).parent.parent.parent
     SRC_DIR = ROOT_DIR / "src"
     DEBUG = True
